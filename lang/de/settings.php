d6a3ad894901ef95761bfe269d9c497777168d53
Anika Henke <anika@selfthinker.org>
2013-01-20 23:35:24 +0000

    updated German language files

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,5 +3,9 @@
  * German language file for Doogies blue Dokuwiki template configuration
  */
 
-$lang['tabsPage']     = 'Name der Seite für die Reiternavigation (leer lassen, um Funktion zu deaktivieren)';
-$lang['actionsToTop'] = 'Die Werkzeuge nach oben verschieben?';
+$lang['tabsPage']           = 'Name der Seite für die Reiternavigation (leer lassen, um Funktion zu deaktivieren)';
+$lang['actionsToTop']       = 'Wo soll die Werkzeugleiste hin?';
+  $lang['actionsToTop_o_0'] = 'Nach unten';
+  $lang['actionsToTop_o_1'] = 'Nach oben';
+  $lang['actionsToTop_o_2'] = 'Nach oben und nach unten';
+$lang['hasLongTitles']      = 'Löse Design-Probleme für Wikis mit langen Seitennamen';
