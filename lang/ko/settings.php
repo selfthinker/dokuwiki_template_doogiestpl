0eb4ef388404b790da246895ea59f537ba55a1c2
Myeongjin <aranet100@gmail.com>
2016-04-21 15:19:26 +0900

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Korean language file for Doogies blue Dokuwiki template configuration
+ */
+
+$lang['tabsPage']           = '탭 둘러보기를 위한 문서 이름 (탭을 비활성화하려면 비워 두세요)';
+$lang['actionsToTop']       = '어디에 동작 모음을 놓겠습니까?';
+  $lang['actionsToTop_o_0'] = '아래에';
+  $lang['actionsToTop_o_1'] = '위에';
+  $lang['actionsToTop_o_2'] = '위와 아래 둘다';
+$lang['hasLongTitles']      = '긴 문서 이름이 있는 위키의 디자인 문제 고치기';
